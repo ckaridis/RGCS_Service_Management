397c75f045b305f3f5823729a19354f97a136eca
xrhstosdion <xrhstosdion@hotmail.com>
2017-10-12 18:15:39 +0300

    initial commit

---
+++ src/main/java/com/rgcs_motors/RGCS_Service_Management/domain/User.java
@@ -0,0 +1,111 @@
+package com.rgcs_motors.RGCS_Service_Management.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table (name="users")
+public class User implements Serializable{
+
+    @Id
+    @Column (nullable = false, name = "ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(nullable = false, name = "VAT", unique=true)
+    private String vat;
+
+    @Column(nullable = false, name = "name")
+    private String firstName;
+
+    @Column(nullable = false, name = "surname")
+    private String lastName;
+
+    @Column(nullable = false, name = "address")
+    private String address;
+
+    @Column(nullable = false, name = "email", unique=true)
+    private String email;
+
+    @Column(nullable = false, name = "password")
+    private String password;
+
+    @Column(nullable = false, name = "user_type")
+    private String type;
+
+    public User () {
+    }
+
+    public User ( String vat, String password, String firstName, String lastName, String address,
+                  String email, String type) {
+
+        this.vat = vat;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.address = address;
+        this.email = email;
+        this.type = type;
+
+    }
+
+    public int getId () {
+        return id;
+    }
+
+    public String getVat () {
+        return vat;
+    }
+
+    public void setVat ( String vat ) {
+        this.vat = vat;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword () {
+        return password;
+    }
+
+    public void setPassword ( String password ) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
---
+++ src/main/resources/templates/index.ftl
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<head lang="en">
+    <title>RGCS MOTORS</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="/css/index.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+	        integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g="
+			 crossorigin="anonymous">
+    </script>
+    <script src="/js/jquery.validate.min.js"></script>
+    <script type="text/javascript" src="/js/index.js"></script>
+</head>
+<body>
+
+   <div id="page_wrapper">
+       <div id="form_wrapper">
+          <form id="loginForm" name="loginForm" action="/loginValidation"  method="post">
+             <div>
+                <p>
+	               <label for="email" class="whiteLabel"><b>Username :</b></label>
+	               <br>
+                   <input type="email" placeholder="Enter Email" id="email" name="email">
+	            </p>
+	            <p>
+                   <label for="password"  class="whiteLabel"><b>Password :</b></label>
+	            <br>
+                   <input type="password" placeholder="Enter Password" id="password" name="password">
+                </p>
+	           <p>
+                  <button type="submit">Login</button>
+	           </p>
+            </div>
+         </form>
+       </div>
+       <div id="hero_wrapper">
+          <h1>RGCS MOTORS.</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sollicitudin rhoncus mi id ornare. Mauris luctus, tortor vel sagittis rhoncus, turpis augue interdum eros, sit amet iaculis sapien velit sed turpis. Donec eu enim nibh. Integer orci nibh, volutpat ac magna quis, pulvinar pretium diam. Mauris facilisis pretium dignissim. Duis vestibulum elit non augue suscipit elementum. Aenean ut justo tellus.
+          </p>
+       </div>
+       <div id="names_list_wrapper">
+          <ul id="names_list" align="center">
+             <li class="li_padding_10">chris</li>
+             <li class="li_padding_10">chris</li>
+             <li class="li_padding_10">george</li>
+             <br>
+             <br>
+             <li class="li_padding_20">john</li>
+             <li class="li_padding_20">vicky</li>
+          </ul>
+       </div>
+       <div id="technologies_wall">
+       </div>
+  </div>
+
+</body>
+</html>
