bcd257786e10804d5327f6fcf2864e42e197e6b8
geoken <geoken90@gmail.com>
2017-10-16 00:23:59 +0300

    Service entity added

--- src/main/java/com/rgcs_motors/RGCS_Service_Management/domain/Service.java
+++ src/main/java/com/rgcs_motors/RGCS_Service_Management/domain/Service.java
@@ -1,13 +1,35 @@
 package com.rgcs_motors.RGCS_Service_Management.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "services")
 public class Service {
 
+    @Id
+    @Column (nullable = false, name = "ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "VEHICLE_PLATE", nullable = false, unique = true)
     private String licensePlates;
+
+    @Column(name = "registration_date", nullable = false)
     private String repairRegistrationDate;
+
+    @Column(name = "service_date", nullable = false)
     private String repairDate;
+
+    @Column(name = "status", nullable = false)
     private String repairStatus;
+
+    @Column(name = "service_type", nullable = false)
     private String repairType;
+
+    @Column(name = "service_cost", nullable = false)
     private double repairCost;
+
+    @Column(name = "description", nullable = false)
     private String repairDescription;
 
     public String getLicensePlates() {
@@ -24,6 +46,20 @@ public class Service {
 
     public void setRepairRegistrationDate(String repairRegistrationDate) {
         this.repairRegistrationDate = repairRegistrationDate;
+    }
+
+
+    public Service()
+    {}
+    
+    public Service(String licensePlates, String repairRegistrationDate, String repairDate, String repairStatus, String repairType, double repairCost, String repairDescription) {
+        this.licensePlates = licensePlates;
+        this.repairRegistrationDate = repairRegistrationDate;
+        this.repairDate = repairDate;
+        this.repairStatus = repairStatus;
+        this.repairType = repairType;
+        this.repairCost = repairCost;
+        this.repairDescription = repairDescription;
     }
 
     public String getRepairDate() {
@@ -64,5 +100,9 @@ public class Service {
 
     public void setRepairDescription(String repairDescription) {
         this.repairDescription = repairDescription;
+    }
+
+    public int getId() {
+        return id;
     }
 }
